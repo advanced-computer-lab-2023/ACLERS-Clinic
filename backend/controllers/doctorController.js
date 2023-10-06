e5348fcdb3a318af2b11f251d5fce496a555ebbc
omaarrwaell <102100200+omaarrwaell@users.noreply.github.com>
2023-10-06 16:08:35 +0300

    Update doctorController.js

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -7,7 +7,7 @@ const registerDoctor = asyncHandler(async (req, res) => {
     const saltRounds = await bcrypt.genSalt(10); // You can adjust the number of salt rounds for security
     const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
 
-<<<<<<< HEAD
+
   const doctor = await Doctor.create({
     username:req.body.username,
     name:req.body.name,
@@ -69,31 +69,4 @@ if (newAffiliation!=null){
 
 })
 module.exports = {editEmail,registerDoctor}
-=======
-    const doctor = await Doctor.create({
-      username: req.body.username,
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword,
-      dateOfBirth: req.body.dateOfBirth,
-      hourlyRate: req.body.hourlyRate,
-      affiliation: req.body.affiliation,
-      educationalBackground: req.body.educationalBackground,
-    });
 
-    res.status(200).json(doctor);
-  } catch (error) {
-    // Handle errors here
-    console.error(error); // Log the error for debugging purposes
-
-    // Send an appropriate error response
-    res.status(500).json({
-      success: false,
-      message: 'An error occurred while registering the doctor',
-      error: error.message, // You can include the error message in the response
-    });
-  }
-});
-
-module.exports = {registerDoctor}
->>>>>>> 8034460ddcb3f96f1dc8c442c0bbf37eb8a5f228
