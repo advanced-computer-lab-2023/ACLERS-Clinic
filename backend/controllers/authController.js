2c59ac60e8ae429609c817686e8c1e6423d3642c
yousssef032 <youssefmohamed032@yahoo.com>
2023-11-11 15:11:52 +0200

    admin login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -19,7 +19,7 @@ const login = asyncHandler(async (req, res) => {
 
   const doctor = await Doctor.findOne({ email });
   const applicant = await Applicant.findOne({ email });
-  const admin = await Admin.findOne({ email });
+  const admin = await Admin.findOne({ username: email });
   if (patient && (await bcrypt.compare(password, patient.password))) {
     return res.json({
       token: generateToken(patient._id, "patient"),
