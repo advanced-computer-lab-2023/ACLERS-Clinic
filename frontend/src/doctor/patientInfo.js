8c89d24c264dbc274710109599f8d9ed675c872b
omar wael <omarwael33@gmail.com>
2023-11-14 23:58:37 +0200

    dine

--- frontend/src/doctor/patientInfo.js
+++ frontend/src/doctor/patientInfo.js
@@ -8,11 +8,42 @@ const PatientInfo = () => {
   const decodedToken = jwt.decode(token);
   console.log("Decoded Token:", decodedToken);
   const doctorId = decodedToken.id;
-
+  const [isEditing, setIsEditing] = useState(false);
+  const [newHealthRecord, setNewHealthRecord] = useState("");
   const { patientId } = useParams();
   const [patient, setPatient] = useState(null);
   const navigate = useNavigate();
+  const handleSaveHealthRecord = () => {
+    // Call the API to update the health record
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        healthRecord: newHealthRecord,
 
+      }),
+    };
+
+    fetch(`http://localhost:8000/Doctor-home/addHealthRecord?patientId=${patientId}`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle success, maybe update the UI or show a notification
+        console.log("Health record updated successfully:", data);
+        setIsEditing(false);
+   
+
+      })
+      .catch((error) => {
+        console.error("Error updating health record:", error);
+        // Handle error, show an error message to the user
+      });
+  };
+  const handleEditHealthRecord = () => {
+    setIsEditing(true);
+  };
   useEffect(() => {
     const requestOptions = {
       method: "GET",
@@ -58,8 +89,33 @@ const PatientInfo = () => {
       <p>Full Name: {patient.patient.emergencyContact.fullName}</p>
       <p>Mobile Number: {patient.patient.emergencyContact.mobileNumber}</p>
       <h3>Health Record</h3>
-      <p>{patient.patient.healthRecord}</p>
+      <p>{patient.healthRecord}</p>
+      {isEditing ? (
+        <>
+          <textarea
+            value={newHealthRecord}
 
+            onChange={(e) => setNewHealthRecord(e.target.value)}
+          />
+          <button onClick={handleSaveHealthRecord}>Save</button>
+        </>
+      ) : (
+        <>
+          <p>Description: {patient.healthRecord}</p>
+          <button onClick={handleEditHealthRecord}>Edit</button>
+        </>
+      )}
+      <div>
+                    Attachments:
+                    <ul>
+                      {patient.attachments.map((attachment, attachmentIndex) => (
+                        <li key={attachmentIndex}>
+                          <img src={`http://localhost:8000/uploads/${attachment.path.substring(8)}`} style={{ maxWidth: "50%", maxHeight: "50%", objectFit: "contain" }} alt={attachment.filename} />
+
+                        </li>
+                      ))}
+                      </ul>
+            </div>
       <button onClick={handleNavigateToFreeSlots}>FollowUp</button>
     </div>
   );
